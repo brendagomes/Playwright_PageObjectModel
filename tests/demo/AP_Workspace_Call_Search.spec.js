09f5b43b72e4d6e5850315ced6eed3a1ff530f0e
BRENDA GOMES <brenda@kredx.com>
2023-08-31 18:33:03 +0530

    commit 31-8 18:32

--- tests/demo/AP_Workspace_Call_Search.spec.js
+++ tests/demo/AP_Workspace_Call_Search.spec.js
@@ -1,5 +1,4 @@
 import { test, expect } from '@playwright/test';
-
 test('test', async ({ page }) => {
   await page.goto('https://biz-dev1-auth.kredx.com/realms/Shared/protocol/openid-connect/auth?response_type=code&client_id=shared-client&state=https://biz-dev1.kredx.com/payables/&redirect_uri=https://biz-dev1.kredx.com/login_callback');
   await page.getByLabel('Email').fill('nikhil+abcde3002f@kredx.com');
@@ -9,19 +8,7 @@ test('test', async ({ page }) => {
   await page.getByLabel('Password').press('CapsLock');
   await page.getByLabel('Password').fill('Asdf!234');
   await page.getByLabel('Password').press('Enter');
-  await page.getByRole('link', { name: 'Activities' }).click();
   await page.getByPlaceholder('Search').click();
   await page.getByPlaceholder('Search').press('CapsLock');
   await page.getByPlaceholder('Search').fill('A_241');
-//await page.pause();
-   await page.locator('#apollo-table-27aebb48-69ac-4979-8671-244ffe326446 > div > div.ant-table-body > table > tbody > tr:nth-child(4) > td.ant-table-cell.ant-table-selection-column.ant-table-cell-fix-left > label > span > input').check();
-   await page.pause();
-   await page.locator(getByLabel('', { exact: true })).check();
-    await page.pause();
-  await page.locator('u').filter({ hasText: 'Dispute Resolution' }).click();
-  await page.locator('div').filter({ hasText: /^StatusOPEN$/ }).getByAltText('edit-icon').click();
-  await page.getByText('Open').click();
-  await page.getByText('InProgress').click();
-  await page.getByRole('button', { name: 'Update' }).click();
-
 });
