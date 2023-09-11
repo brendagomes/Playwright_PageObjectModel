a2c8350c54cbe6208e391cb8106201076e00c156
BRENDA GOMES <brenda@kredx.com>
2023-09-11 18:08:16 +0530

    commit 11-9 18:08

---
+++ tests/demo/AP_My_Disputes_Search.spec.js
@@ -0,0 +1,15 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+    await page.goto('https://biz-dev1-auth.kredx.com/realms/Shared/protocol/openid-connect/auth?response_type=code&client_id=shared-client&state=https://biz-dev1.kredx.com/payables/&redirect_uri=https://biz-dev1.kredx.com/login_callback');
+    await page.getByLabel('Email').fill('nikhil+abcde3002f@kredx.com');
+    await page.getByLabel('Password').click();
+    await page.getByLabel('Password').press('CapsLock');
+    await page.getByLabel('Password').fill('A');
+    await page.getByLabel('Password').press('CapsLock');
+    await page.getByLabel('Password').fill('Asdf!234');
+    await page.getByLabel('Password').press('Enter');
+    await page.getByRole('link', { name: 'Disputes' }).click();
+    await page.getByPlaceholder('Search').click();
+    await page.getByPlaceholder('Search').fill('dispute');
+});
