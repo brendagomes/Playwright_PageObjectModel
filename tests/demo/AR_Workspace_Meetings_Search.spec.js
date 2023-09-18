c3af7c41355d3aad6f2dfb07d97fb157e1f57ddb
BRENDA GOMES <brenda@kredx.com>
2023-09-18 17:05:47 +0530

    commit 18-9 17:05

---
+++ tests/demo/AR_Workspace_Meetings_Search.spec.js
@@ -0,0 +1,20 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://biz-dev1-auth.kredx.com/realms/Shared/protocol/openid-connect/auth?response_type=code&client_id=shared-client&state=https://biz-dev1.kredx.com/receivables/&redirect_uri=https://biz-dev1.kredx.com/login_callback');
+  await page.getByLabel('Email').click({
+    modifiers: ['Control']
+  });
+  await page.getByLabel('Email').fill('nikhil+abcde3002f@kredx.com');
+  await page.getByLabel('Password').click();
+  await page.getByLabel('Password').press('CapsLock');
+  await page.getByLabel('Password').fill('A');
+  await page.getByLabel('Password').press('CapsLock');
+  await page.getByLabel('Password').fill('Asdf!234');
+  await page.getByLabel('Password').press('Enter');
+  await page.getByRole('tab', { name: 'Meetings' }).click();
+  await page.getByPlaceholder('Search').click();
+  await page.getByPlaceholder('Search').press('CapsLock');
+  await page.getByPlaceholder('Search').fill('A_214');
+  //await page.pause();
+});
