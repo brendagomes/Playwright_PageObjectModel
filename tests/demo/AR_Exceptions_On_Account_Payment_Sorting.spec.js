4c2484247591708f2f4b9423a9ad7c184654516d
BRENDA GOMES <brenda@kredx.com>
2023-09-18 18:18:50 +0530

    18-9 18:18

---
+++ tests/demo/AR_Exceptions_On_Account_Payment_Sorting.spec.js
@@ -0,0 +1,19 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://biz-dev1-auth.kredx.com/realms/Shared/protocol/openid-connect/auth?response_type=code&client_id=shared-client&state=https://biz-dev1.kredx.com/receivables/&redirect_uri=https://biz-dev1.kredx.com/login_callback');
+  await page.getByLabel('Email').click({
+    modifiers: ['Control']
+  });
+  await page.getByLabel('Email').fill('nikhil+abcde3002f@kredx.com');
+  await page.getByLabel('Password').click();
+  await page.getByLabel('Password').press('CapsLock');
+  await page.getByLabel('Password').fill('A');
+  await page.getByLabel('Password').press('CapsLock');
+  await page.getByLabel('Password').fill('Asdf!234');
+  await page.getByLabel('Password').press('Enter');
+  await page.getByRole('tab', { name: 'Exceptions' }).click();
+  await page.getByRole('tab', { name: 'On Account Payment' }).click();
+  await page.getByText('Payment Date').click();
+  //await page.pause();
+});
